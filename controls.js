8028e9aedb645c3e2682ca66adf686dfd53faa1c
Andrio Pacheco <andriopacheco@gmail.com>
2023-12-08 19:38:23 -0300

    script pt2

--- controls.js
+++ controls.js
@@ -1,5 +1,5 @@
 export default {
-    jump: new KeyboardEvent('keydown', {key: 'space', keyCode: 21}),
+    jump: new KeyboardEvent('keydown', {key: 'Space', keyCode: 32}),
     dispatch(event){
         document.dispatchEvent(this[event]);
     }
